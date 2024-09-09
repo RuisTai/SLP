ec5dde631e7baa6c84f8f5ebec84e3db849d99cc
Ruis Tai <102351587+RuisTai@users.noreply.github.com>
2024-09-09 23:35:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -186,6 +186,7 @@ if st.button("Predict Stress Level"):
 
     # Display the predicted stress level below the input
     st.session_state.predicted_stress_level = f"Predicted Stress Level: {stress_level} (Level {prediction})"
+    st.session_state.suggestion = f"Meditation, massage, and a warm shower before bed can help you reduce stress when sleeping. Have a SWEET DREAM <3"
     
     # Add the chat bubble above the correct section
     fig.add_annotation(
@@ -226,6 +227,7 @@ if st.button("Predict Stress Level"):
 # Main area for visualization
 if 'predicted_stress_level' in st.session_state:
     st.subheader(st.session_state.predicted_stress_level)
+    st.subheader(st.session_state.suggestion)
 else:
     # Display the initial bar chart in Streamlit
     st.plotly_chart(fig)
